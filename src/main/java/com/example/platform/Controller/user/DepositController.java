62a95c29d8faa45c3e5b5414132cd5e82df5df52
hamisi <hamisishehe@gmail.com>
2025-04-12 07:53:26 +0300

    v12

--- src/main/java/com/example/platform/Controller/user/DepositController.java
+++ src/main/java/com/example/platform/Controller/user/DepositController.java
@@ -133,7 +133,7 @@ public class DepositController {
 
 
     @CrossOrigin(
-            origins = {"https://cryptowealthsolutionscws.com/auth/login", "https://cwsadmin.netlify.app/login"}, // Specify exact origins
+            origins = {"https://cryptowealthsolutionscws.com", "https://cwsadmin.netlify.app"}, // Specify exact origins
             allowedHeaders = {"Content-Type", "Authorization", "X-Requested-With"}, // Limit headers to necessary ones
             methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT}, // Allow only required methods
             allowCredentials = "true", // Keep this only if you need credentials (cookies, authentication)
